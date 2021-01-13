e3d1a34bbf763b4e6214d29170d4f56165f7b424
Dmitry <brothersds@ngs.ru>
2021-01-14 00:07:31 +0300

    continue doing task 6

--- task_4_6.py
+++ task_4_6.py
@@ -7,3 +7,24 @@
 Например, в первом задании выводим целые числа, начиная с 3. При достижении числа 10 — завершаем цикл.
 Вторым пунктом необходимо предусмотреть условие, при котором повторение элементов списка прекратится.
 '''
+
+from itertools import count, cycle
+import sys
+
+
+def my_count(start_number, end_number):
+    for el in count(start_number):
+        if el > end_number:
+            break
+        else:
+            yield el
+
+
+if 'h' in sys.argv[1:]:
+    print('Введите начальное и конечное целые числа через пробел')
+else:
+    start_number, end_number = sys.argv[1:]
+    my_list = [el for el in my_count(int(start_number), int(end_number))]
+    print(my_list)
+
+
