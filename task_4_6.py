e07c99a2046ea390dcf5cdd0bbe3a7bf5badf8ad
Dmitry <brothersds@ngs.ru>
2021-01-14 00:52:39 +0300

    cont doing task6

--- task_4_6.py
+++ task_4_6.py
@@ -12,19 +12,27 @@ from itertools import count, cycle
 import sys
 
 
-def my_count(start_number, end_number):
-    for el in count(start_number):
-        if el > end_number:
+def my_count(start, end):
+    for el in count(start):
+        if el > end:
             break
         else:
             yield el
 
 
+
 if 'h' in sys.argv[1:]:
-    print('Введите начальное и конечное целые числа через пробел')
+    print('Введите начальное и конечное целые числа списка и число его повторений через пробел')
 else:
-    start_number, end_number = sys.argv[1:]
-    my_list = [el for el in my_count(int(start_number), int(end_number))]
-    print(my_list)
+    start_number, end_number, end_cycle = sys.argv[1:]
+    # start_number, end_number = sys.argv[1:]
+# start_number, end_number, end_cycle = 1, 10, 10
+    my_list1 = [el for el in my_count(int(start_number), int(end_number))]
+    print(my_list1)
 
-
+    user_count = 0
+    for el in cycle(my_list1):
+        if user_count > int(end_cycle) - 1:
+            break
+        print(el)
+        user_count += 1
